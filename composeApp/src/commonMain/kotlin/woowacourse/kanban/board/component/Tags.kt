49f526ee3b1438f0e9b289980e73dce7fd131772
홍성우 <98575563+Redish03@users.noreply.github.com>
2026-03-06 11:24:38 +0900

    [1단계 - 칸반 보드 태스크(카드)] 사무엘 미션 제출합니다. (#9)
    * feat: study 테스트 코드 추가
    
    * test: 레벨 0 테스트 작성
    
    * feat: TextExample 구현
    
    * feat: ImageExample 구현
    
    * feat: ButtonExample 구현
    
    * feat: LayoutExample 구현
    
    * chore: icons 의존성 추가
    
    * refactor: 우테코 아이콘에서 하트모양 아이콘으로 변경
    
    * chore: icons 의존성 추가
    
    * docs: 기능 구현 사항 작성
    
    * feat(App.kt): 카드 구현
    
    * feat(App.kt): TaskCard 파라미터 추가
    
    * feat(App.kt): Title 폰트 크기 및 색상 설정
    
    * feat(App.kt): title 최대 1줄 제한, 말줄임표 구현
    
    * feat(App.kt): script 구현
    
    * feat(App.kt): 프로필에 아이콘, 담당자 UI 구현
    
    * feat(App.kt): 담당자 닉네임 최대 줄 수, 말 줄임표 구현
    
    * feat(App.kt): tag 글자수 범위 구현 및 UI 구현
    
    * feat(App.kt): tag 개수 제한 및 공간이 부족할 경우 다음 줄로 배치 구현
    
    * feat(App.kt): 구분선 구현
    
    * refactor(component): 컴포넌트 분리
    
    * feat(App.kt): script, tags 디폴트 파라미터 null로 지정
    
    * refactor(Tags.kt): UI 함수 분리
    
    * refactor: 색상 상수로 분리
    
    * refactor: 불필요한 라이브러리 제거
    
    * feat(App.kt): 프리뷰 추가
    
    * docs: 오타 수정
    
    * feat(App.kt): CheckerView 추가

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/Tags.kt
@@ -0,0 +1,52 @@
+package woowacourse.kanban.board.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.kanban.board.CustomColor
+
+@Composable
+fun Tags(tags: List<String>?) {
+    if (tags != null) {
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            val tagsCount = if(tags.size > 5) 5 else tags.size
+            for (i in 0 until tagsCount) {
+                val filteredTag = if (tags[i].length > 5) tags[i].substring(0 until 5) else tags[i]
+                TagBox(filteredTag)
+            }
+        }
+    }
+}
+
+@Composable
+fun TagBox(filteredTag: String) {
+    Box(
+        modifier = Modifier
+            .background(
+                color = CustomColor.TAG_BACKGROUND.color,
+                shape = RoundedCornerShape(14.dp),
+            )
+            .padding(vertical = 4.dp, horizontal = 6.dp)
+        ,
+    ) {
+        Text(
+            text = filteredTag,
+            fontSize = 12.sp,
+            color = CustomColor.TAG_TEXT.color,
+        )
+    }
+}
