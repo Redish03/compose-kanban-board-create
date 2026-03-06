185a862c0b0356e658deb376cbbd03b8b005869c
홍성우 <98575563+Redish03@users.noreply.github.com>
2026-03-06 11:24:38 +0900

    [1단계 - 칸반 보드 태스크(카드)] 사무엘 미션 제출합니다. (#9)
    * feat: study 테스트 코드 추가
    
    * test: 레벨 0 테스트 작성
    
    * feat: TextExample 구현
    
    * feat: ImageExample 구현
    
    * feat: ButtonExample 구현
    
    * feat: LayoutExample 구현
    
    * chore: icons 의존성 추가
    
    * refactor: 우테코 아이콘에서 하트모양 아이콘으로 변경
    
    * chore: icons 의존성 추가
    
    * docs: 기능 구현 사항 작성
    
    * feat(App.kt): 카드 구현
    
    * feat(App.kt): TaskCard 파라미터 추가
    
    * feat(App.kt): Title 폰트 크기 및 색상 설정
    
    * feat(App.kt): title 최대 1줄 제한, 말줄임표 구현
    
    * feat(App.kt): script 구현
    
    * feat(App.kt): 프로필에 아이콘, 담당자 UI 구현
    
    * feat(App.kt): 담당자 닉네임 최대 줄 수, 말 줄임표 구현
    
    * feat(App.kt): tag 글자수 범위 구현 및 UI 구현
    
    * feat(App.kt): tag 개수 제한 및 공간이 부족할 경우 다음 줄로 배치 구현
    
    * feat(App.kt): 구분선 구현
    
    * refactor(component): 컴포넌트 분리
    
    * feat(App.kt): script, tags 디폴트 파라미터 null로 지정
    
    * refactor(Tags.kt): UI 함수 분리
    
    * refactor: 색상 상수로 분리
    
    * refactor: 불필요한 라이브러리 제거
    
    * feat(App.kt): 프리뷰 추가
    
    * docs: 오타 수정
    
    * feat(App.kt): CheckerView 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,12 +1,22 @@
 package woowacourse.kanban.board
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,29 +26,106 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import kanbanboard.composeapp.generated.resources.Res
 import kanbanboard.composeapp.generated.resources.compose_multiplatform
 import org.jetbrains.compose.resources.painterResource
+import woowacourse.kanban.board.component.Profile
+import woowacourse.kanban.board.component.Script
+import woowacourse.kanban.board.component.Tags
+import woowacourse.kanban.board.component.Title
 
 @Composable
-@Preview
+@Preview(showBackground = true)
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
+    FlowRow(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(38.dp),
+        maxItemsInEachRow = 4,
+        horizontalArrangement = Arrangement.spacedBy(52.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+    ) {
+        TaskCard(
+            title = "LazyColumn 컴포넌트 구현",
+            script = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+            tags = listOf("컴포넌트", "성능"),
+            nickname = "다이노",
+        )
+        TaskCard(
+            title = "LazyColumn 컴포넌트 구현",
+            tags = listOf("컴포넌트", "성능"),
+            nickname = "다이노",
+        )
+        TaskCard(
+            title = "LazyColumn 컴포넌트 구현",
+            script = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+            nickname = "다이노",
+        )
+        TaskCard(
+            title = "LazyColumn 컴포넌트 구현",
+            nickname = "다이노",
+        )
+        TaskCard(
+            title = "너무 너무 긴 제목은 한 줄 까지만 노출시킵니다.",
+            script = "너무 너무 너무 긴 설명은 두 줄까지만 노출하고 말 줄임표로 처리합니다. 두 줄 까지만 노출합니다.",
+            tags = listOf("너무너무", "긴 태그", "최대로", "5자까지", "5개제한임"),
+            nickname = "다이노",
+        )
+    }
+}
+
+@Composable
+fun TaskCard(
+    title: String,
+    script: String? = null,
+    tags: List<String>? = null,
+    nickname: String,
+) {
+    Card(
+        modifier = Modifier
+            .width(286.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+        border = BorderStroke(1.dp, CustomColor.CARD_BORDER.color),
+        shape = RoundedCornerShape(10.dp),
+    ) {
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
+                .padding(17.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
-            }
+            Title(title)
+            Script(script)
+            Tags(tags)
+            HorizontalDivider(
+                thickness = 1.dp,
+                color = CustomColor.DIVIDER.color
+            )
+            Profile(nickname)
         }
+    }
+}
+
+@Composable
+fun CheckerScreen() {
+    var checked by remember { mutableStateOf(true) }
+
+    CheckerView(checked = checked) {
+        checked = !checked
+    }
+}
+
+@Composable
+fun CheckerView(checked: Boolean, check: () -> Unit) {
+    Column {
+        Checkbox(
+            checked = checked,
+            onCheckedChange = { check() },
+        )
+        if(checked) Text(text = "체크됨!")
     }
 }
