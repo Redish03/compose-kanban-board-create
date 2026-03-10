08781bb77fa277a5da9ebcf20cd28c8f343cd894
홍성우 <98575563+Redish03@users.noreply.github.com>
2026-03-10 13:07:06 +0900

    [2단계 - 칸반 보드 태스크(리팩터링)] 사무엘 미션 제출합니다. (#39)
    * refactor: TaskCard 컴포넌트 파일 분리
    
    * refactor: Title 컴포넌트 수정
    
    * refactor: modifier 주입으로 변경
    
    * feat: 각 컴포넌트에 Preview 추가
    
    * test(ComposeTestRule.kt): 컴포즈 테스트 코드 작성
    
    * test(ComposeTestRule.kt): testTag 테스트
    
    * feat(Task.kt): 할 일(Task) 데이터 클래스 추가
    
    * feat(TaskUIMapper.kt): UI 그리는 로직 분리
    
    * refactor(TaskBackground.kt): 배경 컴포넌트 분리
    
    * refactor(App.kt): UI 로직 분리
    
    * refactor(Task.kt): 디폴트 파라미터 넣음
    
    * feat(App.kt): Task 값 넣기
    
    * refactor: Task 값들 nullable에서 nullable 하지 않은 값으로 리팩터링
    
    * feat: TaskCard 데이터를 App.kt에서 생성하는 것으로 옮김
    
    * refactor(Tags.kt): 하드코딩에서 API 활용 코드로 변경
    
    * refactor(Tags.kt): Modifier 주입으로 변경
    
    * feat(Task.kt): 할 일 제목과 이름은 빈칸 일 수 없는 규칙 추가
    
    * refactor: Task 프로퍼티 변수 명 변경
    
    * feat(Profile.kt): 프로필 프리뷰 코드 추가
    
    * docs(README.md): 리드미에 기능 및 리팩터링 방향 추가
    
    * feat(Tags.kt): 프리뷰 수정
    
    * feat(TaskCard.kt): 빈칸 뿐만 아니라 공백이 포함될 경우 UI를 생성하지 않는 방향으로 수정
    
    * test: TaskCard 테스트 추가
    
    * test: Task에 대한 테스트 추가
    
    * test: TaskUIMapper에 대한 테스트 추가
    
    * refactor: 파라미터 이름을 더 책임이 잘 드러나도록 수정
    
    * refactor: 코틀린 컨벤션에 맞게 수정

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,113 +1,51 @@
 package woowacourse.kanban.board
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowRow
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import kanbanboard.composeapp.generated.resources.Res
-import kanbanboard.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
-import woowacourse.kanban.board.component.Profile
-import woowacourse.kanban.board.component.Script
-import woowacourse.kanban.board.component.Tags
-import woowacourse.kanban.board.component.Title
+import woowacourse.kanban.board.component.TaskBackground
+import woowacourse.kanban.board.data.Task
 
 @Composable
 @Preview(showBackground = true)
 fun App() {
-    FlowRow(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(38.dp),
-        maxItemsInEachRow = 4,
-        horizontalArrangement = Arrangement.spacedBy(52.dp),
-        verticalArrangement = Arrangement.spacedBy(20.dp),
-    ) {
-        TaskCard(
-            title = "LazyColumn 컴포넌트 구현",
-            script = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+    val tasks = listOf<Task>(
+        Task(
+            "LazyColumn 컴포넌트 구현",
+            "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+            listOf("컴포넌트", "성능"),
+            "다이노",
+        ),
+        Task(
+            taskTitle = "LazyColumn 컴포넌트 구현",
             tags = listOf("컴포넌트", "성능"),
             nickname = "다이노",
-        )
-        TaskCard(
-            title = "LazyColumn 컴포넌트 구현",
-            tags = listOf("컴포넌트", "성능"),
+        ),
+        Task(
+            taskTitle = "LazyColumn 컴포넌트 구현",
+            taskScript = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
             nickname = "다이노",
-        )
-        TaskCard(
-            title = "LazyColumn 컴포넌트 구현",
-            script = "세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다.",
+        ),
+        Task(
+            taskTitle = "LazyColumn 컴포넌트 구현",
             nickname = "다이노",
-        )
-        TaskCard(
-            title = "LazyColumn 컴포넌트 구현",
-            nickname = "다이노",
-        )
-        TaskCard(
-            title = "너무 너무 긴 제목은 한 줄 까지만 노출시킵니다.",
-            script = "너무 너무 너무 긴 설명은 두 줄까지만 노출하고 말 줄임표로 처리합니다. 두 줄 까지만 노출합니다.",
+        ),
+        Task(
+            taskTitle = "너무 너무 긴 제목은 한 줄 까지만 노출시킵니다.",
+            taskScript = "너무 너무 너무 긴 설명은 두 줄까지만 노출하고 말 줄임표로 처리합니다. 두 줄 까지만 노출합니다.",
             tags = listOf("너무너무", "긴 태그", "최대로", "5자까지", "5개제한임"),
             nickname = "다이노",
-        )
-    }
-}
-
-@Composable
-fun TaskCard(
-    title: String,
-    script: String? = null,
-    tags: List<String>? = null,
-    nickname: String,
-) {
-    Card(
-        modifier = Modifier
-            .width(286.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White,
         ),
-        border = BorderStroke(1.dp, CustomColor.CARD_BORDER.color),
-        shape = RoundedCornerShape(10.dp),
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(17.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp)
-        ) {
-            Title(title)
-            Script(script)
-            Tags(tags)
-            HorizontalDivider(
-                thickness = 1.dp,
-                color = CustomColor.DIVIDER.color
-            )
-            Profile(nickname)
-        }
-    }
+    )
+
+    // Task UI를 생성하는 로직을 Background에 람다로 전달했지만, 더 좋은 방법은 없을까? 람다까지 해야할까?
+    TaskBackground { TaskUIMapper().createTaskUI(tasks) }
 }
 
 @Composable
@@ -126,6 +64,6 @@ fun CheckerView(checked: Boolean, check: () -> Unit) {
             checked = checked,
             onCheckedChange = { check() },
         )
-        if(checked) Text(text = "체크됨!")
+        if (checked) Text(text = "체크됨!")
     }
 }
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/Tags.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/Tags.kt
@@ -10,38 +10,41 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import woowacourse.kanban.board.CustomColor
 
 @Composable
-fun Tags(tags: List<String>?) {
-    if (tags != null) {
+fun Tags(tags: List<String>, tagsModifier: Modifier = Modifier) {
+    if (tags != emptyList<String>()) {
         FlowRow(
             horizontalArrangement = Arrangement.spacedBy(8.dp),
             verticalArrangement = Arrangement.spacedBy(4.dp),
-            modifier = Modifier
-                .fillMaxWidth()
+            modifier = tagsModifier,
         ) {
-            val tagsCount = if(tags.size > 5) 5 else tags.size
-            for (i in 0 until tagsCount) {
-                val filteredTag = if (tags[i].length > 5) tags[i].substring(0 until 5) else tags[i]
-                TagBox(filteredTag)
+            tags.take(5).forEach { tag ->
+                TagBox(
+                    tag.take(5),
+                    Modifier
+                        .background(
+                            color = CustomColor.TAG_BACKGROUND.color,
+                            shape = RoundedCornerShape(14.dp),
+                        ).padding(vertical = 4.dp, horizontal = 6.dp)
+                        .testTag("tag_item"),
+                )
             }
         }
     }
 }
 
 @Composable
-fun TagBox(filteredTag: String) {
+fun TagBox(filteredTag: String, tagsModifier: Modifier = Modifier) {
     Box(
-        modifier = Modifier
-            .background(
-                color = CustomColor.TAG_BACKGROUND.color,
-                shape = RoundedCornerShape(14.dp),
-            )
-            .padding(vertical = 4.dp, horizontal = 6.dp)
-        ,
+        modifier = tagsModifier,
     ) {
         Text(
             text = filteredTag,
@@ -49,4 +52,66 @@ fun TagBox(filteredTag: String) {
             color = CustomColor.TAG_TEXT.color,
         )
     }
+}
+
+// 원래 함수가 바뀔 때마다 Preview 함수도 바꿔야함.
+@Composable
+@Preview
+private fun TagsPreview(@PreviewParameter(TagsPreviewProvider::class) tags: List<String>, tagsModifier: Modifier = Modifier) {
+    if (tags != emptyList<String>()) {
+        FlowRow(
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = tagsModifier,
+        ) {
+            tags.take(5).forEach { tag ->
+                TagBox(
+                    tag.take(5),
+                    Modifier
+                        .background(
+                            color = CustomColor.TAG_BACKGROUND.color,
+                            shape = RoundedCornerShape(14.dp),
+                        ).padding(vertical = 4.dp, horizontal = 6.dp)
+                        .testTag("tag_item"),
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+private fun TagBoxPreview(@PreviewParameter(TagBoxFilteredTagPreviewProvider::class) filteredTag: String) {
+    Box(
+        modifier = Modifier
+            .background(
+                color = CustomColor.TAG_BACKGROUND.color,
+                shape = RoundedCornerShape(14.dp),
+            )
+            .padding(vertical = 4.dp, horizontal = 6.dp),
+    ) {
+        Text(
+            text = filteredTag,
+            fontSize = 12.sp,
+            color = CustomColor.TAG_TEXT.color,
+        )
+    }
+}
+
+private class TagsPreviewProvider : PreviewParameterProvider<List<String>?> {
+    override val values = sequenceOf(
+        listOf("tag1", "tags 테스트", "여러 개의 태그"),
+        listOf("하나의 태그"),
+        listOf("", " ", "  공백 확인  ", "  앞공백", "뒤공백  "),
+    )
+}
+
+private class TagBoxFilteredTagPreviewProvider : PreviewParameterProvider<String> {
+    override val values = sequenceOf<String>(
+        "태그1",
+        "태그 2",
+        "태그 3",
+        "너무너무긴태그",
+        "최대다섯자",
+    )
 }
