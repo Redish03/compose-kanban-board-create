c140fe050ad1fb7f98506a17c79b358fa5107030
홍성우 <98575563+Redish03@users.noreply.github.com>
2026-03-10 13:07:06 +0900

    [2단계 - 칸반 보드 태스크(리팩터링)] 사무엘 미션 제출합니다. (#39)
    * refactor: TaskCard 컴포넌트 파일 분리
    
    * refactor: Title 컴포넌트 수정
    
    * refactor: modifier 주입으로 변경
    
    * feat: 각 컴포넌트에 Preview 추가
    
    * test(ComposeTestRule.kt): 컴포즈 테스트 코드 작성
    
    * test(ComposeTestRule.kt): testTag 테스트
    
    * feat(Task.kt): 할 일(Task) 데이터 클래스 추가
    
    * feat(TaskUIMapper.kt): UI 그리는 로직 분리
    
    * refactor(TaskBackground.kt): 배경 컴포넌트 분리
    
    * refactor(App.kt): UI 로직 분리
    
    * refactor(Task.kt): 디폴트 파라미터 넣음
    
    * feat(App.kt): Task 값 넣기
    
    * refactor: Task 값들 nullable에서 nullable 하지 않은 값으로 리팩터링
    
    * feat: TaskCard 데이터를 App.kt에서 생성하는 것으로 옮김
    
    * refactor(Tags.kt): 하드코딩에서 API 활용 코드로 변경
    
    * refactor(Tags.kt): Modifier 주입으로 변경
    
    * feat(Task.kt): 할 일 제목과 이름은 빈칸 일 수 없는 규칙 추가
    
    * refactor: Task 프로퍼티 변수 명 변경
    
    * feat(Profile.kt): 프로필 프리뷰 코드 추가
    
    * docs(README.md): 리드미에 기능 및 리팩터링 방향 추가
    
    * feat(Tags.kt): 프리뷰 수정
    
    * feat(TaskCard.kt): 빈칸 뿐만 아니라 공백이 포함될 경우 UI를 생성하지 않는 방향으로 수정
    
    * test: TaskCard 테스트 추가
    
    * test: Task에 대한 테스트 추가
    
    * test: TaskUIMapper에 대한 테스트 추가
    
    * refactor: 파라미터 이름을 더 책임이 잘 드러나도록 수정
    
    * refactor: 코틀린 컨벤션에 맞게 수정

---
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/TaskBackground.kt
@@ -0,0 +1,23 @@
+package woowacourse.kanban.board.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TaskBackground(generateTask: @Composable () -> Unit) {
+    FlowRow(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(38.dp),
+        maxItemsInEachRow = 4,
+        horizontalArrangement = Arrangement.spacedBy(52.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp),
+    ) {
+        generateTask()
+    }
+}
