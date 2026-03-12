15272b2dbdc89820b08b1fdb368218597bf9a871
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-12 17:16:42 +0900

    test: 버튼 활성화 여부 테스트 작성

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/newTeskCreate/CreateNewTaskDialogTest.kt
@@ -0,0 +1,33 @@
+package woowacourse.kanban.board.component.newTeskCreate
+
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsEnabled
+import androidx.compose.ui.test.assertIsNotEnabled
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performTextInput
+import androidx.compose.ui.test.runComposeUiTest
+import org.junit.Test
+import woowacourse.kanban.board.component.newTaskCreate.CreateNewTaskDialog
+
+@OptIn(ExperimentalTestApi::class)
+class CreateNewTaskDialogTest {
+    @Test
+    fun `제목이 비어있으면 생성 버튼이 비활성화 된다`() = runComposeUiTest {
+        setContent {
+            CreateNewTaskDialog()
+        }
+
+        onNodeWithText("생성").assertIsNotEnabled()
+    }
+
+    @Test
+    fun `제목이 있다면 생성 버튼이 활성화 된다`() = runComposeUiTest {
+        setContent {
+            CreateNewTaskDialog()
+        }
+
+        onNodeWithTag("title_textField").performTextInput("Hello")
+        onNodeWithText("생성").assertIsEnabled()
+    }
+}
