cbb9aff0b32f6c3d9cecc097470141ee31413956
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-12 17:16:26 +0900

    test: 태그 개수 및 제목 검증 로직 테스트

---
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/newTeskCreate/NewTaskFormTest.kt
@@ -0,0 +1,30 @@
+package woowacourse.kanban.board.component.newTeskCreate
+
+import org.junit.Test
+import woowacourse.kanban.board.component.newTaskCreate.validateTagsAndWordCount
+import woowacourse.kanban.board.component.newTaskCreate.validateTitle
+import kotlin.test.assertEquals
+
+class NewTaskFormTest {
+    @Test
+    fun `제목이 null 또는 공백인 경우 제목을 입력해달라는 안내 문구 반환`() {
+        val title1 = ""
+        val title2 = "  "
+        val title3 = null
+
+        val answerString = "제목을 입력해 주세요."
+
+        assertEquals(answerString, validateTitle(title1))
+        assertEquals(answerString, validateTitle(title2))
+        assertEquals(answerString, validateTitle(title3))
+    }
+
+    @Test
+    fun `태그의 개수가 5개가 넘어가는 경우 포맷에 맞춰 입력해달라는 안내 문구 반환`() {
+        val tags = "조디악,사무엘,호이,앨리,아키,허닛"
+
+        val answerString = "태그는 5자 이내로 5개까지만 등록할 수 있습니다."
+
+        assertEquals(answerString, validateTagsAndWordCount(tags))
+    }
+}
