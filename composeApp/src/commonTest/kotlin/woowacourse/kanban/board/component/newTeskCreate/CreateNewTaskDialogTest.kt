4dd1b0a11f6f6ad866f6a11d2b973b72dc480f32
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-17 01:16:59 +0900

    test: 유효하지 않은 값이 들어올 경우 에러메세지를 띄우는 UI 테스트 시나리오 추가

--- composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/newTeskCreate/CreateNewTaskDialogTest.kt
+++ composeApp/src/commonTest/kotlin/woowacourse/kanban/board/component/newTeskCreate/CreateNewTaskDialogTest.kt
@@ -1,9 +1,12 @@
 package woowacourse.kanban.board.component.newTeskCreate
 
 import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
-import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.hasAnyAncestor
+import androidx.compose.ui.test.hasSetTextAction
+import androidx.compose.ui.test.hasTestTag
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performTextInput
 import androidx.compose.ui.test.runComposeUiTest
@@ -22,12 +25,73 @@ class CreateNewTaskDialogTest {
     }
 
     @Test
+    fun `제목에 공백이 주어지면 생성 버튼이 비활성화 된다`() = runComposeUiTest {
+        setContent {
+            CreateNewTaskDialog()
+        }
+
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("title_textField")))
+            .performTextInput("")
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("title_textField")))
+            .performTextInput("  ")
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("title_textField")))
+            .performTextInput("\n\t")
+        onNodeWithText("생성").assertIsNotEnabled()
+    }
+
+    @Test
     fun `제목이 있다면 생성 버튼이 활성화 된다`() = runComposeUiTest {
         setContent {
             CreateNewTaskDialog()
         }
 
-        onNodeWithTag("title_textField").performTextInput("Hello")
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("title_textField")))
+            .performTextInput("입력 테스트")
         onNodeWithText("생성").assertIsEnabled()
     }
+
+    @Test
+    fun `제목이 있어도 태그에 유효하지 않은 값이 입력되면 버튼이 비활성화 된다`() = runComposeUiTest {
+        setContent {
+            CreateNewTaskDialog()
+        }
+
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("title_textField")))
+            .performTextInput("입력 테스트")
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("tag_textField")))
+            .performTextInput("hello,world,")
+        onNodeWithText("생성").assertIsNotEnabled()
+    }
+
+    @Test
+    fun `태그를 5개 초과하여 등록하려 하면 에러메세지를 띄운다`() = runComposeUiTest {
+        setContent {
+            CreateNewTaskDialog()
+        }
+        val input = "test1, test2, test3, test4, test5, test6"
+
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("tag_textField")))
+            .performTextInput(input)
+
+        onNodeWithText("태그는 5자 이내로 5개까지만 등록할 수 있습니다.")
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun `형식에 맞지 않는 태그를 입력 시 에러메세지를 띄운다`() = runComposeUiTest {
+        setContent {
+            CreateNewTaskDialog()
+        }
+        val input = "test1, test2,"
+
+        onNode(hasSetTextAction() and hasAnyAncestor(hasTestTag("tag_textField")))
+            .performTextInput(input)
+
+        onNodeWithText("태그는 5자 이내로 5개까지만 등록할 수 있습니다.")
+            .assertIsDisplayed()
+    }
+
+//    @Test
+//    fun `제목을 입력했다가 지우면 에러메세지를 띄운다`() = runComposeUiTest {
+//    } 이건 어떻게 하지?
 }
