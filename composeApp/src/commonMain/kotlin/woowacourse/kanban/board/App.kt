2ef61bda5eee9c7fcd37d63e6d8289e362d2db1a
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-19 17:11:34 +0900

    feat: Task에 현재 상태 추가

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,8 +1,5 @@
 package woowacourse.kanban.board
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,6 +12,7 @@ import woowacourse.kanban.board.data.Tags
 import woowacourse.kanban.board.data.Task
 import woowacourse.kanban.board.data.Title
 import woowacourse.kanban.board.data.Nickname
+import woowacourse.kanban.board.data.TaskStatus
 
 @Composable
 fun App() {
@@ -51,26 +49,31 @@ val tasksExample = listOf<Task>(
         Title("LazyColumn 컴포넌트 구현"),
         Script("세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다."),
         Tags(listOf("컴포넌트", "성능")),
+        TaskStatus.TO_DO,
         Nickname("다이노"),
     ),
     Task(
         taskTitle = Title("LazyColumn 컴포넌트 구현"),
         tags = Tags(listOf("컴포넌트", "성능")),
+        status = TaskStatus.TO_DO,
         nickname = Nickname("다이노"),
     ),
     Task(
         taskTitle = Title("LazyColumn 컴포넌트 구현"),
         taskScript = Script("세로 스크롤 가능한 리스트 컴포넌트를 만들고 성능 최적화를 적용합니다."),
+        status = TaskStatus.TO_DO,
         nickname = Nickname("다이노"),
     ),
     Task(
         taskTitle = Title("LazyColumn 컴포넌트 구현"),
+        status = TaskStatus.TO_DO,
         nickname = Nickname("다이노"),
     ),
     Task(
         taskTitle = Title("너무 너무 긴 제목은 한 줄 까지만 노출시킵니다."),
         taskScript = Script("너무 너무 너무 긴 설명은 두 줄까지만 노출하고 말 줄임표로 처리합니다. 두 줄 까지만 노출합니다."),
         tags = Tags(listOf("너무너무", "긴 태그", "최대로", "5자까지", "5개제한임")),
+        status = TaskStatus.TO_DO,
         nickname = Nickname("다이노"),
     ),
 )
--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/data/Task.kt
@@ -4,6 +4,7 @@ data class Task(
     val taskTitle: Title,
     val taskScript: Script = Script(""),
     val tags: Tags = Tags(emptyList()),
+    val status: TaskStatus,
     val nickname: Nickname,
 )
 
