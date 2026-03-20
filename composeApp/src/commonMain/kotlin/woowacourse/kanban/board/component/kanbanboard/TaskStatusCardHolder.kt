a168b6997defbd4deaabf7cff697143ae6db82ff
Redish03 <pluto0303@pukyong.ac.kr>
2026-03-21 02:34:20 +0900

    feat: CardHolder를 스크롤이 가능하도록 구현한다.

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/kanbanboard/TaskStatusCardHolder.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/component/kanbanboard/TaskStatusCardHolder.kt
@@ -2,6 +2,8 @@ package woowacourse.kanban.board.component.kanbanboard
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.rememberScrollableState
+import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.FlowRow
@@ -10,7 +12,9 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -110,7 +114,8 @@ fun TaskCardHolder(
     modifier: Modifier = Modifier,
 ) {
     FlowRow(
-        modifier = modifier,
+        modifier = modifier
+            .verticalScroll(rememberScrollState()),
         maxItemsInEachRow = 1,
         horizontalArrangement = Arrangement.Center,
         verticalArrangement = Arrangement.spacedBy(20.dp),
